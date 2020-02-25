d6795135049ed4864de029beb053aaa6d313070d
Jorge Antonio Ramirez Medina <jorgeramirez@grupovidanta.com>
2020-02-25 17:15:10 -0600

    Se respalda proyecto

--- Angular/SurveyAdministrationSystem/src/app/pages/dashboard/dashboard.component.ts
+++ Angular/SurveyAdministrationSystem/src/app/pages/dashboard/dashboard.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { SquareServices } from 'src/app/services/square-services';
 import { Square } from 'src/app/models/square';
+import { Hotel } from 'src/app/models/hotel';
+import { AreasServices } from 'src/app/services/areas-services';
+import { Area } from 'src/app/models/area';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,14 +13,17 @@ import { Square } from 'src/app/models/square';
 export class DashboardComponent implements OnInit {
 
   squares: Array<Square>;
+  hotels: Array<Hotel>;
+  areas: Array<Area>;
+
   constructor(
-    private _squaresServices: SquareServices
-  ) { 
-    this.squares = this._squaresServices.getAllSquares();
-  }
+    private _squaresServices: SquareServices,
+    private _areasServices: AreasServices
+  ) { }
 
   ngOnInit() {
-    
+    this.squares = this._squaresServices.getAllSquares();
+    this.hotels = this._squaresServices.getAllHotels();
+    this.areas = this._areasServices.getAllAreas();
   }
-
 }
