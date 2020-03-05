0d43a4c5982f50df761a5cd7722c7eb4199ef2f1
Jorge Antonio Ramirez Medina <jorgeramirez@grupovidanta.com>
2020-03-04 23:07:53 -0600

    Se respalda proyecto

--- Angular/SurveyAdministrationSystem/src/app/pages/catalogs/departments/departments.component.ts
+++ Angular/SurveyAdministrationSystem/src/app/pages/catalogs/departments/departments.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, Renderer2 } from '@angular/core';
 import { Department } from 'src/app/models/department';
 import { DepartmentsServices } from 'src/app/services/departments.services';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-departments',
@@ -15,9 +16,12 @@ export class DepartmentsComponent implements OnInit {
   newButtonHidden: boolean;
   departments: Array<Department>;
   departmentSelected: Department;
+  originalName: string;
+  @ViewChild('departmentName', { read: ElementRef }) departmentName: ElementRef;
 
   constructor(
-    private _departmentsServices: DepartmentsServices
+    private _departmentsServices: DepartmentsServices,
+    private _renderer: Renderer2
   ) { }
 
   ngOnInit() {
@@ -25,6 +29,8 @@ export class DepartmentsComponent implements OnInit {
     this.cancelButtonHidden = true;
     this.inputNameDisabled = true;
     this.newButtonHidden = false;
+    this.originalName = "";
+    this.departmentSelected = new Department();
 
     this.departments = this._departmentsServices.getAll();
   }
@@ -41,6 +47,10 @@ export class DepartmentsComponent implements OnInit {
   }
 
   restartScreen() {
+    if(this.departmentSelected.id > 0) {
+      this.departmentSelected.name = this.originalName;
+    }
+
     this.departmentSelected = new Department();
     this.saveButtonHidden = true;
     this.cancelButtonHidden = true;
@@ -53,14 +63,41 @@ export class DepartmentsComponent implements OnInit {
     }
   }
 
+  onClickRow(row: HTMLElement) {
+    this.selectRow(row);
+  }
+
   edit(department: Department, row: HTMLElement) {
     this.departmentSelected = department;
     this.enableEditControls();
+    this.selectRow(row);
+    this.originalName = this.departmentSelected.name;
+  }
 
+  selectRow(currentCow: HTMLElement) {
     let rows = document.getElementsByClassName("selected");
     if(rows.length > 0) {
       rows[0].classList.remove("selected");
     }
-    row.classList.add("selected");
+    currentCow.classList.add("selected");
+  }
+
+  onClickDepartmentName() {
+    this._renderer.removeClass(this.departmentName.nativeElement, "alert-danger");
+  }
+
+  save(departmentName: string) {
+    if(departmentName.length == 0) {
+      this._renderer.addClass(this.departmentName.nativeElement, "alert-danger");
+
+      Swal.fire({
+        title: 'Datos incompletos',
+        text: 'Debe capturar el nombre del departamento',
+        icon: 'warning'
+      });
+    }
+    else {
+
+    }
   }
 }
