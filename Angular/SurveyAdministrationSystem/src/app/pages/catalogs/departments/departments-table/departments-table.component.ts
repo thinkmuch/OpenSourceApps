4cf277cf1f2fcc8b4feda7b27cb1d7f8cc0d525c
Jorge <jorgeramirez@grupovidanta.com>
2020-03-06 23:37:49 -0600

    Se respalda proyecto

--- Angular/SurveyAdministrationSystem/src/app/pages/catalogs/departments/departments-table/departments-table.component.ts
+++ Angular/SurveyAdministrationSystem/src/app/pages/catalogs/departments/departments-table/departments-table.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Department } from 'src/app/models/department';
+import Swal from 'sweetalert2';
+import { Status } from 'src/app/enums/class-enum';
+import { DepartmentsServices } from 'src/app/services/departments.services';
 
 @Component({
   selector: 'app-departments-table',
@@ -8,11 +11,71 @@ import { Department } from 'src/app/models/department';
 })
 export class DepartmentsTableComponent implements OnInit {
 
-  @Input("departmentsInput") departments: Array<Department>
+  @Input("departmentsInput") departments: Array<Department>;
+  @Output() editEvent: EventEmitter<{ department: Department, row: HTMLElement }>;
+  @Output() clickRowEvent: EventEmitter<{ department: Department, row: HTMLElement }>;
 
-  constructor() { }
+  constructor(
+    private _departmentsServices: DepartmentsServices
+  ) { 
+    this.editEvent = new EventEmitter<{department: Department, row: HTMLElement}>();
+    this.clickRowEvent = new EventEmitter<{ department: Department, row: HTMLElement }>();
+  }
 
   ngOnInit() {
   }
 
+  enable(department: Department) {
+    Swal.fire({
+      title: 'Activar',
+      text: `¿Seguro que desea activar el departamento ${department.name}?`,
+      icon: 'question',
+      showCancelButton: true,
+      confirmButtonText: 'Si, desactivar',
+      cancelButtonText: 'Cancelar'
+    }).then((response) => {
+      
+      if(response.value) {
+
+        department.status = Status.Active;
+        this._departmentsServices.update(department);
+        
+        Swal.fire({
+          title: 'Departamento activado',
+          icon: 'success'
+        });
+      }
+    });
+  }
+
+  disable(department: Department) {
+    Swal.fire({
+      title: 'Desactivar',
+      text: `¿Seguro que desea desactivar el departamento ${department.name}?`,
+      icon: 'question',
+      showCancelButton: true,
+      confirmButtonText: 'Si, desactivar',
+      cancelButtonText: 'Cancelar'
+    }).then((response) => {
+      
+      if(response.value) {
+
+        department.status = Status.Inactive;
+        this._departmentsServices.update(department);
+        
+        Swal.fire({
+          title: 'Departamento desactivado',
+          icon: 'success'
+        });
+      }
+    });
+  }
+
+  edit(department: Department, row: HTMLElement) {
+    this.editEvent.emit({ department: department, row: row });
+  }
+
+  onClickRow(row: HTMLElement, department: Department) {
+    this.clickRowEvent.emit({ department: department, row: row });
+  }
 }
