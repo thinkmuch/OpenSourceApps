6daa9315697636fa455f3e5d2cb0b7fd7422eb9b
Jorge <jorgeramirez@grupovidanta.com>
2020-03-08 18:38:25 -0600

    Se respalda proyecto

--- Angular/SurveyAdministrationSystem/src/app/pages/catalogs/cruises/cruise-detail/cruise-detail.component.ts
+++ Angular/SurveyAdministrationSystem/src/app/pages/catalogs/cruises/cruise-detail/cruise-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Site } from 'src/app/models/site';
+import { SitesServices } from 'src/app/services/sites-service';
+import { Department } from 'src/app/models/department';
+import { DepartmentsServices } from 'src/app/services/departments.services';
 
 @Component({
   selector: 'app-cruise-detail',
@@ -7,9 +11,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CruiseDetailComponent implements OnInit {
 
-  constructor() { }
+  sites: Array<Site>;
+  departments: Array<Department>;
+
+  constructor(
+    private _sitesServices: SitesServices,
+    private _departmentServices: DepartmentsServices
+  ) { }
 
   ngOnInit() {
+    this.sites = this._sitesServices.getAll();
+    this.departments = this._departmentServices.getAll();
   }
 
 }
