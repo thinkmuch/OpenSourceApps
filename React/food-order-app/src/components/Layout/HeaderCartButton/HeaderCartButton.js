59e258bcd66629ef03e3996677507a5bef357d62
thinkmuch <think_much@hotmail.com>
2022-01-25 21:33:31 -0600

    Use Context
    Context is added to manage the state of the app.

--- React/food-order-app/src/components/Layout/HeaderCartButton/HeaderCartButton.js
+++ React/food-order-app/src/components/Layout/HeaderCartButton/HeaderCartButton.js
@@ -1,7 +1,14 @@
+import { useContext } from 'react';
 import CartIcon from '../../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css'
+import CartContext from '../../../store/cart-context';
 
 const HeaderCartButton = (props) => {
+    const cartContext = useContext(CartContext);
+    const numberOfCartItems = cartContext.items.reduce((curNumber, item) => {
+        return curNumber + item.amount;
+    }, 0);
+
     return (
         <button className={classes.button} onClick={props.onClick}>
             <span>
@@ -11,7 +18,7 @@ const HeaderCartButton = (props) => {
                 Your Cart
             </span>
             <span className={classes.badge}>
-                3
+                {numberOfCartItems}
             </span>
         </button>
     );
--- React/food-order-app/src/components/Meals/MealItem/MealItem.js
+++ React/food-order-app/src/components/Meals/MealItem/MealItem.js
@@ -1,8 +1,20 @@
+import { useContext } from 'react';
 import classes from './MealItem.module.css';
 import MealItemForm from './MealItemForm/MealItemForm';
+import CartContext from '../../../store/cart-context';
 
 const MealItem = (props) => {
+    const cartCtx = useContext(CartContext)
     const price = `$${props.price.toFixed(2)}`;
+
+    const addToCartHandler = (amount) => {
+        cartCtx.addItem({
+            id: props.id,
+            name: props.name,
+            amount: amount,
+            price: props.price
+        });
+    };
 
     return (
         <li className={classes.meal}>
@@ -16,7 +28,7 @@ const MealItem = (props) => {
                 </div>
             </div>
             <div>
-                <MealItemForm id={props.id}/>
+                <MealItemForm id={props.id} onAddToCart={addToCartHandler}/>
             </div>
         </li>
     );
