46b32a2ed0b1a0e5674816cc9ed90f20f890e87a
thinkmuch <think_much@hotmail.com>
2022-01-26 20:45:36 -0600

    Finish touches
    Finish touches added

--- React/food-order-app/src/components/Cart/Cart.js
+++ React/food-order-app/src/components/Cart/Cart.js
@@ -9,8 +9,13 @@ const Cart = (props) => {
     const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
     const hasItems = cartCtx.items.length > 0;
 
-    const cartItemRemoveHandler = (id) => {};
-    const cartItemAddHandler = (item) => {};
+    const cartItemRemoveHandler = (id) => {
+        cartCtx.removeItem(id);
+    };
+
+    const cartItemAddHandler = (item) => {
+        cartCtx.addItem({...item, amount: 1});
+    };
 
     const cartItems = <ul className={classes['cart-items']}>
         {
@@ -21,7 +26,7 @@ const Cart = (props) => {
                     amount={item.amount} 
                     price={item.price}
                     onRemove={cartItemRemoveHandler.bind(null, item.id)}
-                    onAdd={cartItemRemoveHandler.bind(null, item)}/>
+                    onAdd={cartItemAddHandler.bind(null, item)}/>
             ))
         }
     </ul>;
--- React/food-order-app/src/components/Layout/HeaderCartButton/HeaderCartButton.js
+++ React/food-order-app/src/components/Layout/HeaderCartButton/HeaderCartButton.js
@@ -1,16 +1,36 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import CartIcon from '../../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css'
 import CartContext from '../../../store/cart-context';
 
 const HeaderCartButton = (props) => {
+    const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
     const cartContext = useContext(CartContext);
-    const numberOfCartItems = cartContext.items.reduce((curNumber, item) => {
+
+    const { items } = cartContext;
+    const numberOfCartItems = items.reduce((curNumber, item) => {
         return curNumber + item.amount;
     }, 0);
 
+    const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`;
+
+    useEffect(() => {
+        if (cartContext.items.length === 0) {
+            return;
+        }
+        setBtnIsHighlighted(true);
+
+        const timer = setTimeout(() => {
+            setBtnIsHighlighted(false);
+        }, 300);
+
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [items]);
+
     return (
-        <button className={classes.button} onClick={props.onClick}>
+        <button className={btnClasses} onClick={props.onClick}>
             <span>
                 <CartIcon/>
             </span>
